25e185a469e9cb8454e255df4bb2210616c17d3c
Dalu Alozie <dalualozie1@gmail.com>
2022-11-21 20:07:18 +0000

    blog improvements

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import { useState, useEffect } from 'react'
 import Post from '../components/blog/post'
-import { collection, getDocs, query, DocumentData, orderBy} from "firebase/firestore"; 
+import { doc,collection, getDocs, query, DocumentData, orderBy} from "firebase/firestore"; 
 import db from '../firebase/config';
 import { PostType } from '../components/blog/props';
 
@@ -9,8 +9,7 @@ interface Props {
   
 }
 
-const PostsRef = collection(db, "Posts");
-const getPostQuery = query(PostsRef, orderBy("timeStamp"));
+
 
 
 const Blog: NextPage = () => {
@@ -25,14 +24,17 @@ const Blog: NextPage = () => {
 
   //Fetches posts from server
   const getPosts = async () => {
+    // const q = query(collection(db, "Posts"),orderBy("timeStamp"));
 
-    const res = await getDocs(getPostQuery);
-    const docs = res.docs.map((doc) => {doc.data()}).reverse()
-    setPosts(docs)        
+    // const res = await getDocs(q);
+    // const docs = res.docs.map((doc) => doc.data()).reverse();
+    // console.log(docs)
+    // setPosts(docs)   
+         
   }
 
   return (
-  <div className='w-full h-full flex flex-col items-center'>
+  <div className='flex flex-col items-center w-full h-full'>
     {
       posts.map((post: PostType) => 
       <Post key={post.timeStamp.nanoseconds+post.timeStamp.seconds} 
