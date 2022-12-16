9cd1c3e33bc4c7265664f14c7d49956ab7f2db6f
Dalu Alozie <dalualozie1@gmail.com>
2022-12-16 15:14:29 +0000

    tryna fix nav

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -9,7 +9,7 @@ import campus from '../web_assets/web_assets/campus.svg'
 const Contact: NextPage = () => {
   const cellClass = "flex flex-col justify-between min-h-max content-center text-center my-24 w-1/4 h-max"
   return (
-    <div className='w-screen h-screen min-h-screen bg-white min-w-max'>
+    <div className='w-full h-screen min-h-screen bg-white min-w-max'>
       <div className='flex flex-col justify-between w-screen bg-white pb-52 min-h-max min-w-max'>
         <div className='w-full contact-us-banner'>
           <div className='mt-16 text-center home-message'>
