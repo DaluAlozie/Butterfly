e9d524a1be551d21d92250141cb99ef79894897b
Dalu Alozie <dalualozie1@gmail.com>
2022-12-16 16:00:01 +0000

    ahhh

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -16,64 +16,7 @@ const Contact: NextPage = () => {
             <b>Contact Us</b>
           </div>
         </div> */}
-        <div className='flex flex-row content-center justify-between w-full px-1 align-middle bg-white min-h-max'>
-          <div className={cellClass}>
-            <Image src={email}
-                      height={120}
-                      width={120}
-                      alt="Logo"
-                      >
-            </Image>  
-            <div className='mt-1 mb-2'>Email us at</div>
-            <div>
-              admin@butterfly-social.com
-            </div>
-
-          </div>
-          <div className={cellClass}>
-            <Image src={socialMedia}
-                      height={120}
-                      width={120}
-                      alt="Logo"
-                      >
-            </Image> 
-            
-            <div className='mt-1 mb-5'> Follow us on</div>
-            <div className='flex flex-row content-center self-center w-1/2 justify-evenly min-w-max min-h-max'>
-              <a className='mx-5' href="https://www.instagram.com/butterfly.warwick/" target="_blank" rel="noopener noreferrer">
-                  <Image src={insta}
-                    height={40}
-                    width={40}
-                    alt="Logo"
-                    >
-                </Image>  
-                </a>      
-                <a className='mx-5' href="https://www.linkedin.com/company/butterflytechnologies/" target="_blank" rel="noopener noreferrer">
-                  <Image src={linkedinIcon}
-                    height={40}
-                    width={40}
-                    alt="Logo"
-                    >
-                  </Image>  
-                </a>   
-            </div>
-          </div>
-          <div className={cellClass}>
-            <Image src={campus}
-                      height={120}
-                      width={120}
-                      alt="Logo"
-                      >
-            </Image>
-            
-            <div className='my-5'>Visit us at </div>
-            <div>
-              <div>Butterfly Technologies, Warwick University,</div>
-              <div>Gibbet Hill Rd, Coventry,</div>
-              <div>CV4 7ES, United Kingdom</div>
-            </div>
-          </div>
-        </div>
+  
       </div>
 
     </div>
