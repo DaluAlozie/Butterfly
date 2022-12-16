37d7dc0f9acaa19eece8961928d91292a04ab2ae
Dalu Alozie <dalualozie1@gmail.com>
2022-12-16 16:22:31 +0000

    fixed hopefully

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -7,15 +7,15 @@ import email from '../web_assets/web_assets/email.svg'
 import campus from '../web_assets/web_assets/campus.svg'
 
 const Contact: NextPage = () => {
-  const cellClass = "flex flex-col justify-between min-h-max content-center text-center my-24 w-36 h-max bg-green-200"
+  const cellClass = "flex flex-col justify-between min-h-max content-center text-center my-24 w-40 h-max"
   return (
     <div className='w-full h-screen min-h-screen bg-white min-w-max'>
       <div className='flex flex-col justify-between w-full bg-white min-w-max pb-52 min-h-max'>
-        {/* <div className='w-full contact-us-banner'>
+        <div className='w-full contact-us-banner'>
           <div className='mt-16 text-center home-message'>
             <b>Contact Us</b>
           </div>
-        </div> */}
+        </div>
   
       </div>
       <div className='flex flex-row w-full bg-white min-h-max justify-evenly'>
