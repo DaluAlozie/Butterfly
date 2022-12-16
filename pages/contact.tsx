66dfd2fdc48910f752facd07f449279ad73feb7e
Dalu Alozie <dalualozie1@gmail.com>
2022-12-16 16:07:26 +0000

    almost

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -7,7 +7,7 @@ import email from '../web_assets/web_assets/email.svg'
 import campus from '../web_assets/web_assets/campus.svg'
 
 const Contact: NextPage = () => {
-  const cellClass = "flex flex-col justify-between min-h-max content-center text-center my-24 w-1/3 h-max"
+  const cellClass = "flex flex-col justify-between min-h-max content-center text-center my-24 w-1/12 h-max"
   return (
     <div className='w-full h-screen min-h-screen bg-white min-w-max'>
       <div className='flex flex-col justify-between w-full bg-white min-w-max pb-52 min-h-max'>
@@ -18,22 +18,49 @@ const Contact: NextPage = () => {
         </div> */}
   
       </div>
-      <div className='flex flex-row content-center justify-between w-full px-1 align-middle bg-white min-h-max'>
-      <div className={cellClass}>
-        <Image src={email}
-                height={120}
-                width={120}
-                alt="Logo"
-                >
-        </Image>  
-        <div className='mt-1 mb-2'>Email us at</div>
-        <div>
-        admin@butterfly-social.com
+      <div className='flex flex-row content-center justify-around w-full px-1 align-middle bg-white min-h-max'>
+        <div className={cellClass}>
+          <Image src={email}
+                  height={120}
+                  width={120}
+                  alt="Logo"
+                  >
+          </Image>  
+          <div className='mt-1 mb-2'>Email us at</div>
+          <div>
+          admin@butterfly-social.com
+          </div>
+
         </div>
-
-</div>
+        <div className={cellClass}>
+          <Image src={socialMedia}
+                    height={120}
+                    width={120}
+                    alt="Logo"
+                    >
+          </Image> 
+          
+          <div className='mt-1 mb-5'> Follow us on</div>
+          <div className='flex flex-row content-center self-center w-1/2 justify-evenly min-w-max min-h-max'>
+            <a className='mx-5' href="https://www.instagram.com/butterfly.warwick/" target="_blank" rel="noopener noreferrer">
+                <Image src={insta}
+                  height={40}
+                  width={40}
+                  alt="Logo"
+                  >
+              </Image>  
+              </a>      
+              <a className='mx-5' href="https://www.linkedin.com/company/butterflytechnologies/" target="_blank" rel="noopener noreferrer">
+                <Image src={linkedinIcon}
+                  height={40}
+                  width={40}
+                  alt="Logo"
+                  >
+                </Image>  
+              </a>   
+          </div>
+        </div>
       </div>
-
     </div>
 
   )
