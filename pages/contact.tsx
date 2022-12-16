35bbfb5290921929ceef6cbf7b1c908381257ba8
Dalu Alozie <dalualozie1@gmail.com>
2022-12-16 15:51:28 +0000

    styll

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -9,8 +9,8 @@ import campus from '../web_assets/web_assets/campus.svg'
 const Contact: NextPage = () => {
   const cellClass = "flex flex-col justify-between min-h-max content-center text-center my-24 w-1/3 h-max"
   return (
-    <div className='w-screen h-screen min-h-screen bg-white'>
-      <div className='flex flex-col justify-between w-full bg-white pb-52 min-h-max'>
+    <div className='w-screen h-screen min-h-screen bg-white min-w-fit'>
+      <div className='flex flex-col justify-between w-full bg-white min-w-max pb-52 min-h-max'>
         {/* <div className='w-full contact-us-banner'>
           <div className='mt-16 text-center home-message'>
             <b>Contact Us</b>
